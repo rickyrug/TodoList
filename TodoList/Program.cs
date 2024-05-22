1c7b6e2370205c05815b739f510274703e739158
JOSE RICARDO RUGERIO SAN VICENTE <jrugeriosanvicente@tenaris.com>
2024-05-21 18:15:46 -0600

    .

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,1 +1,19 @@
 ﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee");
+Console.WriteLine("[C]reate");
+Console.WriteLine("[D]elete");
+Console.WriteLine("[E]xit");
+
+
+
+
+string userInput = Console.ReadLine();
+
+Console.WriteLine( userInput);
+
+Console.ReadLine();
+
+
+
+
+
